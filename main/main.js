997253f6003a2f270c7dd2daecc442adc6969a05
isaiah <mticket@hotmail.com>
2019-05-29 10:00:51 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var price = 0;
+	if(inputs.distance <= 2){
+		price = 6 + 0.25 * inputs.parkTime;
+	}else if(inputs.distance <= 8){
+		price = 6 + (inputs.distance - 2) * 0.8 + inputs.parkTime * 0.25;
+	}else{
+		price = 6 + (8 - 2) * 0.8 + (inputs.distance - 8) * 0.8 * 1.5 + inputs.parkTime * 0.25;}
+    return Math.round(price);
 };
